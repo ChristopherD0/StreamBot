0d70eac8bc890a81dc303fd6eabb3d94d45b19e4
ChristopherD0 <xo07l9mgceyj@hotmail.com>
2025-01-14 20:31:12 +0800

    implement twitch api integration

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+const TwitchAPI = require('./lib/twitch');
+
 console.log('StreamBot starting...');
 
-// TODO: Add platform integrations
-// TODO: Add data collection logic
+const twitch = new TwitchAPI();
+
+async function testTwitchAPI() {
+  try {
+    const streamInfo = await twitch.getStreamInfo('shroud');
+    if (streamInfo) {
+      console.log(`${streamInfo.user_name} is live with ${streamInfo.viewer_count} viewers`);
+    } else {
+      console.log('Stream is offline or not found');
+    }
+  } catch (error) {
+    console.error('Twitch API test failed:', error.message);
+  }
+}
+
+// testTwitchAPI();
